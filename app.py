1a99e85331b96897435d4b8290bf8edc92c5215a
Suleiman <Suleimanayomide716@gmail.com>
2024-10-14 13:49:24 +0100

    Update app.py

--- app.py
+++ app.py
@@ -44,4 +44,4 @@ def process_request():
             return jsonify({"error": "Failed to retrieve message"}), kora_response.status_code
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True,host='0.0.0.0.',port=3000)
